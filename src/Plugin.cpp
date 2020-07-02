baae230959eb8d6edc1f9fdb58f5494be2ef7bed
Hannes <hannes@octofox.de>
2020-07-02 13:46:19 +0200

    :construction: WIP vestecWindow in global GUI

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -13,6 +13,7 @@
 #include "../../../src/cs-utils/convert.hpp"
 #include "../../../src/cs-utils/utils.hpp"
 #include "../../../src/cs-utils/logger.hpp"
+#include "../../../src/cs-utils/filesystem.hpp"
 #include "logger.hpp"
 
 #include <VistaKernel/GraphicsManager/VistaSceneGraph.h>
@@ -63,8 +64,30 @@ void Plugin::init() {
       "VESTEC", "whatshot", "../share/resources/gui/vestec_tab.html");
   // mGuiManager->addScriptToSideBarFromJS("../share/resources/gui/js/vestec_sidebar.js");
 
+  logger().debug("UI.css");
+  mGuiManager->addCssToGui("third-party/css/jquery-ui.min.css");
+  logger().debug("vested.css");
+  mGuiManager->addCssToGui("css/vestec.css");
+
+  logger().debug("alight");
+  mGuiManager->addScriptToGuiFromJS("../share/resources/gui/third-party/js/alight.min.js");
+  logger().debug("jquery-ui");
+  mGuiManager->addScriptToGuiFromJS("../share/resources/gui/third-party/js/jquery-ui.min.js");
+  logger().debug("d3-node");
+  mGuiManager->addScriptToGuiFromJS("../share/resources/gui/third-party/js/d3-node-editor.js");
+  logger().debug("vtk.js");
+  mGuiManager->addScriptToGuiFromJS("../share/resources/gui/third-party/js/vtk_13.7.1.js");
+
+  logger().debug("vestec.js");
+  mGuiManager->addScriptToGuiFromJS("../share/resources/gui/js/vestec.js");
+
+  logger().debug("vestec window load");
+  auto vestecWindowHtml = cs::utils::filesystem::loadToString("../share/resources/gui/vestecWindow.html");
+  logger().debug("vestec window add");
+  mGuiManager->getGui()->callJavascript("CosmoScout.gui.addHtml", vestecWindowHtml, "body");
+
   // Adding the main GUI item for VESTEC to cosmoscout
-  m_pVESTEC_UI = new cs::gui::GuiItem("file://../share/resources/gui/vestecWindow.html", 1);
+/*  m_pVESTEC_UI = new cs::gui::GuiItem("file://../share/resources/gui/vestecWindow.html", 1);
   mGuiManager->addGuiItem(m_pVESTEC_UI, 10);
   m_pVESTEC_UI->setRelSizeX(1.0f);
   m_pVESTEC_UI->setRelSizeY(1.0f);
@@ -72,15 +95,15 @@ void Plugin::init() {
   m_pVESTEC_UI->setRelPositionX(0.f);
   m_pVESTEC_UI->setRelOffsetY(-0.45f);
   m_pVESTEC_UI->setRelOffsetX(0.5f);
-  m_pVESTEC_UI->waitForFinishedLoading();
+  m_pVESTEC_UI->waitForFinishedLoading();*/
 
   // Read the plugin settings from the scene config
   mPluginSettings = mAllSettings->mPlugins.at("csp-vestec");
 
   // add anchor node and register to solar system
-  mVestecTransform = std::make_shared<cs::scene::CelestialAnchorNode>(
+/*  mVestecTransform = std::make_shared<cs::scene::CelestialAnchorNode>(
       mSceneGraph->GetRoot(), mSceneGraph->GetNodeBridge(), "", "Earth", "IAU_Earth");
-  mSolarSystem->registerAnchor(mVestecTransform);
+  mSolarSystem->registerAnchor(mVestecTransform);*/
 
   // Initialize and append gui elements
   InitGUI();
@@ -89,7 +112,7 @@ void Plugin::init() {
   Plugin::dataDir = mPluginSettings.mVestecDataDir;
 
   // Initialize vestec flow editor
-  m_pNodeEditor = new VNE::NodeEditor(m_pVESTEC_UI);
+  m_pNodeEditor = new VNE::NodeEditor(mGuiManager->getGui());
 
   // TODO:Create the Node editor
   m_pNodeEditor->RegisterSocketType("CINEMA_DB");
@@ -161,8 +184,8 @@ void Plugin::init() {
 }
 
 void Plugin::deInit() {
-  mSolarSystem->unregisterAnchor(mVestecTransform);
-  mSceneGraph->GetRoot()->DisconnectChild(mVestecTransform.get());
+/*  mSolarSystem->unregisterAnchor(mVestecTransform);
+  mSceneGraph->GetRoot()->DisconnectChild(mVestecTransform.get());*/
   delete m_pNodeEditor;
 }
 
