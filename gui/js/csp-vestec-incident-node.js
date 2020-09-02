14c923a15d9e9e96ab7257dd3163742bd324fd5d
Hannes <hannes@octofox.de>
2020-09-02 13:48:50 +0200

    :hammer: Incident Node refactoring
    - Only show relevant output for dataset type
      - Node ready 'type' property of dataset metadata, 'typeMappings' object maps type to output type
      - :construction: TODO Write data to outputs
    - Load Incidents
    - Load Datasets of incident
    - TODO: Comments / Testing

--- gui/js/csp-vestec-incident-node.js
+++ gui/js/csp-vestec-incident-node.js
@@ -1,6 +1,23 @@
 /* global CosmoScout, $, D3NE */
 
 class IncidentNode {
+  static outputTypes = [
+    'TEXTURES',
+    'CINEMA_DB',
+    'POINT_ARRAY',
+  ];
+
+  static typeMappings = {
+    '2D_FIRE': IncidentNode.outputTypes[0],
+    CINEMA_DB: IncidentNode.outputTypes[1],
+    POINT_ARRAY: IncidentNode.outputTypes[2],
+  }
+
+  constructor() {
+    Object.freeze(IncidentNode.outputTypes);
+    Object.freeze(IncidentNode.typeMappings);
+  }
+
   /**
    * @param node {{data: {}, addControl: Function, addOutput: Function, addInput: Function, id: number|string}}
    * @returns {*}
@@ -10,20 +27,25 @@ class IncidentNode {
     const cinemaDBOutput = new D3NE.Output('Cinema DB', CosmoScout.vestecNE.sockets.CINEMA_DB);
     const pointsOutput = new D3NE.Output('Points', CosmoScout.vestecNE.sockets.POINT_ARRAY);
 
+    node.data.TEXTURES = textureOutput;
+    node.data.CINEMA_DB = cinemaDBOutput;
+    node.data.POINT_ARRAY = pointsOutput;
+
     const incidentControl = new D3NE.Control(
       `<select id="incident_node_select_${node.id}" class="combobox"></select>`,
       (element, control) => {
         $(element).selectpicker();
-        const loaded = IncidentNode.loadIncidents(element);
 
         control.putData('incidentSelect', element);
-        control.putData('incidentsLoaded', loaded);
+        control.putData('incidentSelectContainer', element.parentElement.parentElement);
+        control.putData('incidentsLoaded', false);
 
-        if (!loaded) {
-          element.parentElement.parentElement.classList.add('hidden');
-        }
+        element.parentElement.parentElement.classList.add('hidden');
 
         element.addEventListener('change', (event) => {
+          IncidentNode.unsetNodeValues(node);
+          IncidentNode.showOutputType(node, 'none', true);
+
           node.data.incidentDatasetLoaded = IncidentNode.loadIncidentDatasets(
             node.data.incidentDatasetSelect,
             event.target.value,
@@ -35,35 +57,24 @@ class IncidentNode {
     const incidentDatasetControl = new D3NE.Control(
       `<select id="incident_dataset_node_select_${node.id}" class="combobox"></select>`,
       (element, control) => {
-        control.putData('incidentDatasetSelect', element);
+        $(element).selectpicker();
 
-        if (node.data.incidentsLoaded) {
-          node.data.incidentDatasetLoaded = IncidentNode.loadIncidentDatasets(
-            element,
-            node.data.incidentSelect.value,
-          );
-        } else {
-          control.putData('incidentDatasetSelect', element);
-          $(element).selectpicker();
-          element.parentElement.parentElement.classList.add('hidden');
-        }
+        control.putData('incidentDatasetSelect', element);
+        control.putData('incidentDatasetSelectContainer', element.parentElement.parentElement);
+        control.putData('incidentDatasetLoaded', false);
+
+        element.parentElement.parentElement.classList.add('hidden');
       },
     );
 
     const loginMessageControl = new D3NE.Control(
       `<strong id="incident_node_message_${node.id}">Please login first</strong>`,
       (element, control) => {
-        control.putData('info', element);
+        control.putData('info', element.parentElement);
 
-        if (node.data.incidentsLoaded) {
-          element.parentElement.classList.add('hidden');
-        }
+        element.parentElement.classList.add('hidden');
       },
     );
-
-    node.data.updateOutputs = (type) => {
-      console.log(type);
-    };
 
     node.addControl(loginMessageControl);
     node.addControl(incidentControl);
@@ -73,7 +84,26 @@ class IncidentNode {
     node.addOutput(cinemaDBOutput);
     node.addOutput(pointsOutput);
 
+    node.data.firstWorkerRound = true;
+    CosmoScout.vestecNE.updateEditor();
+
     return node;
+  }
+
+  static handleUnauthorized(node) {
+    node.data.incidentSelectContainer.classList.add('hidden');
+    node.data.incidentDatasetSelectContainer.classList.add('hidden');
+    node.data.info.classList.remove('hidden');
+
+    node.data.incidentsLoaded = false;
+    node.data.incidentDatasetLoaded = false;
+    IncidentNode.unsetNodeValues(node);
+  }
+
+  static handleAuthorized(node) {
+    node.data.incidentSelectContainer.classList.remove('hidden');
+    node.data.incidentDatasetSelectContainer.classList.remove('hidden');
+    node.data.info.classList.add('hidden');
   }
 
   /**
@@ -81,68 +111,81 @@ class IncidentNode {
    * @param _inputs
    * @param outputs
    */
-  worker(node, _inputs, outputs) {
+  async worker(node, _inputs, outputs) {
+    if (node.data.firstWorkerRound) {
+      node.data.firstWorkerRound = false;
+      IncidentNode.showOutputType(node, 'none', false);
+    }
+
     if (!CosmoScout.vestec.isAuthorized()) {
-      node.data.incidentSelect.parentElement.parentElement.classList.add('hidden');
-      node.data.incidentDatasetSelect.parentElement.parentElement.classList.add('hidden');
-      node.data.info.parentElement.classList.remove('hidden');
-
-      node.data.incidentsLoaded = false;
-      node.data.incidentDatasetLoaded = false;
-
-      outputs[0] = {
-        incidentId: undefined,
-        datasetId: undefined,
-      };
+      IncidentNode.handleUnauthorized(node);
 
       return;
     }
 
-    node.data.incidentSelect.parentElement.parentElement.classList.remove('hidden');
-    node.data.incidentDatasetSelect.parentElement.parentElement.classList.remove('hidden');
-    node.data.info.parentElement.classList.add('hidden');
+    IncidentNode.handleAuthorized(node);
 
     if (!node.data.incidentsLoaded) {
-      node.data.incidentsLoaded = IncidentNode.loadIncidents(node.data.incidentSelect);
-    } else if (!node.data.incidentDatasetLoaded) {
-      node.outputs.forEach((output) => {
-        console.log(output.el);
-        // output.el.classList.add('hidden');
-      });
+      node.data.incidentsLoaded = await IncidentNode.loadIncidents(node.data.incidentSelect);
+    }
 
-      node.data.incidentDatasetLoaded = IncidentNode.loadIncidentDatasets(
+    if (node.data.incidentsLoaded && !node.data.incidentDatasetLoaded) {
+      node.data.incidentDatasetLoaded = await IncidentNode.loadIncidentDatasets(
         node.data.incidentDatasetSelect,
         node.data.incidentSelect.value,
       );
     }
 
-    if (node.data.incidentDatasetLoaded) {
+    if (node.data.incidentsLoaded && node.data.incidentDatasetLoaded) {
       const incidentId = node.data.incidentSelect.value;
       const datasetId = node.data.incidentDatasetSelect.value;
 
-      if (typeof incidentId === 'undefined'
-          || typeof datasetId === 'undefined'
-          || incidentId === null
-          || datasetId === null
-          || incidentId.length === 0
-          || datasetId.length === 0
-      ) {
+      if (incidentId.length === 0 || datasetId.length === 0) {
         return;
       }
 
-      CosmoScout.vestec.getIncidentDatasetMetadata(datasetId, incidentId).then((data) => {
-        node.data.updateOutputs(data.type);
-      });
+      const metadata = await IncidentNode.loadIncidentDatasetMetadata(node, datasetId, incidentId);
+
+      node.data.loadedDataHash = datasetId + incidentId;
+      node.data.currentMetadata = metadata;
     }
 
-    if (typeof node.data.incidentSelect !== 'undefined') {
+    /*    if (typeof node.data.incidentSelect !== 'undefined') {
       outputs[0] = {
         incidentId: node.data.incidentSelect.value,
         datasetId: node.data.incidentDatasetSelect.value.length > 0
           ? node.data.incidentDatasetSelect.value
           : undefined,
       };
+    } */
+  }
+
+  static async loadIncidentDatasetMetadata(node, datasetId, incidentId) {
+    if (incidentId === null
+        || datasetId === null
+        || incidentId.length === 0
+        || datasetId.length === 0
+    ) {
+      throw Error('Required ids missing');
     }
+
+    if (node.data.loadedDataHash === datasetId + incidentId) {
+      return node.data.currentMetadata;
+    }
+
+    const metadata = await CosmoScout.vestec
+      .getIncidentDatasetMetadata(datasetId, incidentId)
+      .catch(() => ({
+        type: null,
+      }));
+
+    if (typeof metadata.type !== 'undefined' && metadata.type !== null) {
+      IncidentNode.showOutputType(node, IncidentNode.typeMappings[metadata.type], true);
+
+      return metadata;
+    }
+
+    throw Error('Metadata null');
   }
 
   /**
@@ -166,26 +209,26 @@ class IncidentNode {
    * @param element {HTMLSelectElement}
    * @returns true on success
    */
-  static loadIncidents(element) {
+  static async loadIncidents(element) {
     if (!CosmoScout.vestec.isAuthorized()) {
       console.warn('User not authorized, aborting.');
       return false;
     }
 
-    CosmoScout.vestec.getIncidents().then((incidents) => {
-      $(element).selectpicker('destroy');
-      CosmoScout.gui.clearHtml(element);
+    const incidents = await CosmoScout.vestec.getIncidents();
 
-      incidents.forEach((incident) => {
-        const option = document.createElement('option');
-        option.text = incident.name;
-        option.value = incident.uuid;
+    $(element).selectpicker('destroy');
+    CosmoScout.gui.clearHtml(element);
 
-        element.appendChild(option);
-      });
+    incidents.forEach((incident) => {
+      const option = document.createElement('option');
+      option.text = incident.name;
+      option.value = incident.uuid;
 
-      $(element).selectpicker();
+      element.appendChild(option);
     });
+
+    $(element).selectpicker();
 
     return true;
   }
@@ -197,7 +240,7 @@ class IncidentNode {
    * @param id {string} Unique incident UUID
    * @returns true on success
    */
-  static loadIncidentDatasets(element, id) {
+  static async loadIncidentDatasets(element, id) {
     if (!CosmoScout.vestec.isAuthorized()) {
       console.warn('User not authorized, aborting.');
       return false;
@@ -207,22 +250,63 @@ class IncidentNode {
       return false;
     }
 
-    CosmoScout.vestec.getIncidentDatasets(id).then((dataSets) => {
-      $(element).selectpicker('destroy');
-      CosmoScout.gui.clearHtml(element);
+    const datasets = await CosmoScout.vestec.getIncidentDatasets(id);
 
-      dataSets.forEach((dataset) => {
-        const option = document.createElement('option');
-        option.text = dataset.name;
-        option.value = dataset.uuid;
+    $(element).selectpicker('destroy');
+    CosmoScout.gui.clearHtml(element);
 
-        element.appendChild(option);
-      });
+    datasets.forEach((dataset) => {
+      const option = document.createElement('option');
+      option.text = dataset.name;
+      option.value = dataset.uuid;
 
-      $(element).selectpicker();
+      element.appendChild(option);
     });
 
+    $(element).selectpicker();
+
+    CosmoScout.vestecNE.updateEditor(); // Fucking hack
+
     return true;
+  }
+
+  static showOutputType(node, outputType, removeConnections = true) {
+    if (node.data.activeOutputType === outputType) {
+      return;
+    }
+
+    let filter;
+
+    if (outputType === 'none') {
+      filter = () => true;
+    } else {
+      filter = (type) => type !== outputType
+          && typeof node.data[outputType] !== 'undefined';
+    }
+
+    IncidentNode.outputTypes
+      .filter(filter)
+      .forEach((type) => {
+        node.data[type].el.parentElement.classList.add('hidden');
+      });
+
+    if (removeConnections) {
+      CosmoScout.vestecNE.removeConnections(node);
+    }
+
+    if (typeof node.data[outputType] !== 'undefined') {
+      node.data[outputType].el.parentElement.classList.remove('hidden');
+
+      node.data.activeOutputType = outputType;
+    } else if (outputType !== 'none') {
+      console.error(`Output type ${outputType} not recognized.`);
+    }
+  }
+
+  static unsetNodeValues(node) {
+    node.data.activeOutputType = null;
+    node.data.loadedDataHash = null;
+    node.data.currentMetadata = null;
   }
 
   /**
